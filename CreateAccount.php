73dd98b0231273004222fb01f4215685d62eefc2
bengel0608 <bengel0608@gmail.com>
2018-11-11 15:25:30 -0600

    Update CreateAccount.php
    Adding PHP Database Connection to register users into a Database.

---
+++ CreateAccount.php
@@ -0,0 +1,42 @@
+<?php
+  //session_start();
+  // create short variable names
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  //$_SESSION['username'] = $username;
+?>
+<html>
+<head>
+  <title>Creating Account</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<h1>Creating Account</h1>
+<?php
+
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "root";
+	$db = "practice";
+	
+	$user = '';
+	$passwd = "";
+
+	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+	
+	$options = ['cost' => 7];
+	$phash = password_hash($password, PASSWORD_BCRYPT, $options)."\n";
+		
+	$sql = "INSERT INTO Users (username, password) VALUES('$username', '$phash')";
+		
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+		
+	
+	$conn->close();
+	header('Location: login.html');
+
+?>
