207cd9446c7f73d824ddd0ae1f6795467303cf3e
mpbrennan7 <brennanm@fontbonne.edu>
2018-11-17 17:38:42 -0600

    Added some field catching, will need more for a full db insertion

--- registration.php
+++ registration.php
@@ -15,5 +15,19 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
- else{echo "here";}
+ 	try{
+		error_reporting(0);
+  $name = $_REQUEST['FullName'];
+  $age = $_REQUEST['age'];
+  $email = $_REQUEST['email'];
+  $password = $_REQUEST['password'];
+  $zip = $_REQUEST['zipcode'];
+  $userType = $_REQUEST['UserType'];
+  $income = $_REQUEST['Income'];
+  
+	}
+	catch(Exception $e){
+		
+	}
+  
  ?>
