8d885792a88f85863948abc2a47fee04e2cda576
mpbrennan7 <brennanm@fontbonne.edu>
2018-11-17 17:07:26 -0600

    Set up initial database connection

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+	$server = "golddigger.cl5oeek4fomj.us-east-2.rds.amazonaws.com";
+	$username = "root";
+	$password = "password";
+	$dbname = "Golddigger.com";
+	$port = 3306;
+
+
+$con=mysqli_connect($server,$username,$password,$dbname,$port);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ else{echo "here";}
+ ?>
