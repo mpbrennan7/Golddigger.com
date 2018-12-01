57d1f26e3aca99a3114e5b350e2237a544bf3b76
bengel0608 <bengel0608@gmail.com>
2018-12-01 11:13:52 -0600

    Update CreateAccount.php

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,32 +1,32 @@
 <?php
   //session_start();
   // create short variable names
-  $fullname = $_POST['FullName'];
-  $password = $_POST['Password'];
-  $age = $_POST['age'];
-  $email = $_POST['email'];
-  $zipcode = $_POST['zipcode'];
-  $income = $_POST['Income'];
-  $phoneNum = $_POST['phoneNum'];
-  $type = $_POST['UserType'];
-  //$numCars = $_POST[];
+  $fullname = $_POST['FullName'];//get POST variable and store it in a short variable name
+  $password = $_POST['Password'];//get POST variable and store it in a short variable name
+  $age = $_POST['age'];//get POST variable and store it in a short variable name
+  $email = $_POST['email'];//get POST variable and store it in a short variable name
+  $zipcode = $_POST['zipcode'];//get POST variable and store it in a short variable name
+  $income = $_POST['Income'];//get POST variable and store it in a short variable name
+  $phoneNum = $_POST['phoneNum'];//get POST variable and store it in a short variable name
+  $type = $_POST['UserType'];//get POST variable and store it in a short variable name
+  //$numCars = $_POST[];//get POST variable and store it in a short variable name
   $numCars = 2;
-  $hairColor = $_POST['hair'];
-  $eyeColor = $_POST['eye'];
-  $height = $_POST['height_feet'];
-  $catOrDog = $_POST['catsdogs'];
-  $religious = $_POST['religious'];
-  $ruralOrUrban = $_POST['ruralurban'];
-  $cook = $_POST['cook'];
-  $beachOrSki = $_POST['beachski'];
-  $introvertOrExtrovert = $_POST['introextro'];
-  $genre = $_POST['genre'];
-  $relationshipStatus = $_POST['relationship'];
-  $aboutYourself = $_POST['aboutyourself'];
-  $horoscope = $_POST['horoscope'];
-  $lookingFor = $_POST['lookingfor'];
-  $favoriteCereal = $_POST['cereal'];
-  $shoeSize = $_POST['shoe'];
+  $hairColor = $_POST['hair'];//get POST variable and store it in a short variable name
+  $eyeColor = $_POST['eye'];//get POST variable and store it in a short variable name
+  $height = $_POST['height_feet'];//get POST variable and store it in a short variable name
+  $catOrDog = $_POST['catsdogs'];//get POST variable and store it in a short variable name
+  $religious = $_POST['religious'];//get POST variable and store it in a short variable name
+  $ruralOrUrban = $_POST['ruralurban'];//get POST variable and store it in a short variable name
+  $cook = $_POST['cook'];//get POST variable and store it in a short variable name
+  $beachOrSki = $_POST['beachski'];//get POST variable and store it in a short variable name
+  $introvertOrExtrovert = $_POST['introextro'];//get POST variable and store it in a short variable name
+  $genre = $_POST['genre'];//get POST variable and store it in a short variable name
+  $relationshipStatus = $_POST['relationship'];//get POST variable and store it in a short variable name
+  $aboutYourself = $_POST['aboutyourself'];//get POST variable and store it in a short variable name
+  $horoscope = $_POST['horoscope'];//get POST variable and store it in a short variable name
+  $lookingFor = $_POST['lookingfor'];//get POST variable and store it in a short variable name
+  $favoriteCereal = $_POST['cereal'];//get POST variable and store it in a short variable name
+  $shoeSize = $_POST['shoe'];//get POST variable and store it in a short variable name
   
   echo $fullname;
   echo "<br/>";
@@ -79,38 +79,40 @@
   
   //$_SESSION['username'] = $username;
 ?>
-<html>
-<head>
-  <title>Creating Account</title>
-  <link rel="stylesheet" href="PageStyles.css">
-</head>
-<body>
-<h1>Creating Account</h1>
+<html> <!-- Include HTML for some basis -->
+<head> <!-- Begin HTML Head -->
+  <title>Creating Account</title> <!-- Script is titled creating account in browser -->
+  <link rel="stylesheet" href="PageStyles.css"> <!-- Obtain a stylesheet -->
+</head> <!-- Close HTML Head -->
+<body> <!-- Begin HTML Body -->
+<h1>Creating Account</h1> <!-- Make large header h1 stating that the account is being created -->
 <?php
 
-	$dbhost = "golddigger.cl5oeek4fomj.us-east-2.rds.amazonaws.com";
-	$dbuser = "root";
-	$dbpass = "password";
-	$db = "golddigger";
+	$dbhost = "golddigger.cl5oeek4fomj.us-east-2.rds.amazonaws.com"; //cloud database
+	$dbuser = "root"; //connect with root user
+	$dbpass = "password";//DB password
+	$db = "golddigger";//DB Name
 	
 	$user = '';
 	$passwd = "";
 
-	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);//Attempt connecting to DB
 	
-	$options = ['cost' => 7];
-	$phash = password_hash($password, PASSWORD_BCRYPT, $options)."\n";
+	$options = ['cost' => 7];//set up hashing passwords
+	$phash = password_hash($password, PASSWORD_BCRYPT, $options)."\n";//Prepare hash
 		
+	//Create INSERT statement
 	$sql = "INSERT INTO Users (name, pword, age, email, zip, income, phoneNum, type, numCars, hairColor, eyeColor, height, catOrDog, religious, ruralOrUrban, cook, beachOrSki, introvertOrExtrovert, genre, relationshipStatus, aboutYourself, horoscope, lookingFor, favoriteCereal, shoeSize) VALUES('$fullname', '$phash', $age, '$email', $zipcode, $income, $phoneNum, '$type', $numCars, '$hairColor', '$eyeColor', $height, $catOrDog, $religious, $ruralOrUrban, $cook, $beachOrSki, $introvertOrExtrovert, '$genre', '$relationshipStatus', '$aboutYourself', '$horoscope', '$lookingFor', '$favoriteCereal', $shoeSize)";
 		
+	//If query is successful then a record has been inserted to the DB	
 	if ($conn->query($sql) === TRUE) {
 		echo "New record created successfully";
-	} else {
+	} else {//Otherwise some error was encountered
 		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
 		
 	
-	$conn->close();
+	$conn->close();//close DB connection
 	//header('Location: LoginPage.html');
 
 ?>
